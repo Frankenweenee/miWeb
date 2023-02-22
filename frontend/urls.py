555da78eb768197b13208f5698624b9ae345c521
Frankenweenee <117282793+Frankenweenee@users.noreply.github.com>
2023-02-22 17:31:32 +0100

    change on urls

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from frontend import views
 
 urlpatterns = [
-    path('home/', views.home, name ='home'),
+    path('index/', views.index, name ='index'),
     path('porfolio/', views.portfolio, name ='portfolio'),
     path('about/', views.about, name ='about'),
     path('work/', views.work, name ='work'),
